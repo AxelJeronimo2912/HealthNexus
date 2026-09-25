5b037af3d162a3e671144b6848da2c01d3281f3d
Jesus <susej-gr88@hotmail.com>
2026-09-25 14:02:59 -0600

    Mejorar el formulario de registro de pacientes (selects dependientes estado/municipio

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
-
-
 import Alpine from 'alpinejs';
+import Chart from 'chart.js/auto';
 
 window.Alpine = Alpine;
+window.Chart = Chart;
 
 Alpine.start();
-import Chart from 'chart.js/auto';
-window.Chart = Chart;
